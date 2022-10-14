b0dd7967e00d089f5736d6b237ec79c2853f072e
Fredy Santamaria <fsabtamaria@berryit.net>
2022-10-14 09:42:11 -0400

    Added a MenuBar with Two Menus

--- PDF_Splitter.py
+++ PDF_Splitter.py
@@ -1,6 +1,9 @@
 from tkinter import ttk
 from tkinter import *
 
+def donothing():
+    print('something')
+
 def main_window():
     #Create Window
     root = Tk()
@@ -9,6 +12,24 @@ def main_window():
     #Center Window
     root.eval('tk::PlaceWindow . center')
 
+    #Create menu bar
+    menu_bar = Menu(root)
+
+    #Create file menu
+    file_menu = Menu(menu_bar, tearoff=0)
+    file_menu.add_command(label="Open", command=donothing)
+
+    #Create options menu
+    options_menu = Menu(menu_bar, tearoff=0)
+    # Variable used by the checkbutton
+    split_by_page = BooleanVar()
+    options_menu.add_checkbutton(label='Split by page', onvalue=1, offvalue=0, variable=split_by_page)
+
+    #Add the cascades
+    menu_bar.add_cascade(label='File', menu=file_menu)
+    menu_bar.add_cascade(label='Options', menu=options_menu)
+
+    root.config(menu=menu_bar)
     root.mainloop()
 
 if __name__ == '__main__':
