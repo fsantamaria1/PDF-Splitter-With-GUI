bda1e9653a147bc0463a6a6d346e2fe04a00a2b7
Fredy Santamaria <fsabtamaria@berryit.net>
2022-10-14 10:23:51 -0400

    Created Labels, Buttons, Textboxes, etc

--- PDF_Splitter.py
+++ PDF_Splitter.py
@@ -1,8 +1,9 @@
 from tkinter import ttk
 from tkinter import *
+from tkinter import filedialog
 
 def donothing():
-    print('something')
+    pass
 
 def main_window():
     #Create Window
@@ -30,6 +31,33 @@ def main_window():
     menu_bar.add_cascade(label='Options', menu=options_menu)
 
     root.config(menu=menu_bar)
+
+    #Create labels, buttons, textboxes,etc
+    file_selection_label = Label(root, relief=FLAT, text="Selected File:")
+    file_path_label = Label(root, relief=GROOVE, text="N/A")
+    page_selection_label = Label(root, relief=FLAT, text="Page Selection:")
+    dash_label = Label(root, text="-")
+    first_page_combobox = ttk.Combobox(root, state="readonly", values=["..."], width=10)
+    last_page_combobox = ttk.Combobox(root, state="readonly", values=["..."], width=10)
+    save_file = Button(root, text="Save", command=donothing)
+    cancel = Button(root, text="Cancel", command=donothing)
+
+    first_page_combobox.current(0)
+    last_page_combobox.current(0)
+
+    # Position entry + buttons
+    file_selection_label.grid(row=0, column=0, columnspan=3, padx=15, pady=(15, 0), sticky=W+E+N+S)
+    file_path_label.grid(row=1, column=0, columnspan=3, padx=15, pady=(0, 10), sticky=W+E+N+S)
+    page_selection_label.grid(row=2, column=0, columnspan=3, padx=15, pady=(10, 5), sticky=W+E+N+S)
+    first_page_combobox.grid(row=3, column=0, padx=15, sticky=W+E+N+S)
+    dash_label.grid(row=3, column=1, padx=15, sticky=W+E+N+S)
+    last_page_combobox.grid(row=3, column=2, padx=15, sticky=W+E+N+S)
+
+    # save_file.grid(row=3, column=0, padx=15, pady=(15, 15), columnspan=3, sticky=W+E+N+S)
+    save_file.grid(row=4, column=0, padx=15, pady=(20, 15), columnspan=1, sticky=W+E+N+S)
+    cancel.grid(row=4, column=2, columnspan=1, padx=15, pady=(20, 15), sticky=W+E+N+S)
+
+
     root.mainloop()
 
 if __name__ == '__main__':
